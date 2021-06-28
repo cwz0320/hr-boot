3a3f4e0fe6c34e6a11cb86b680bbdb38e02cedc5
陈威志 <447209833@qq.com>
2021-06-28 15:15:47 +0800

    '创建工程'

---
+++ src/main/java/cwz/demo/hrboot/HrBootApplication.java
@@ -0,0 +1,13 @@
+package cwz.demo.hrboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrBootApplication.class, args);
+    }
+
+}
